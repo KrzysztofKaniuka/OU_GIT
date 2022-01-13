0d1b0fbed13a4aa3a978b1afde00f548d9f9391d
NightRaven <240697@edu.p.lodz.pl>
2022-01-13 20:10:25 +0100

    Hello World

---
+++ cmake-build-debug/CMakeFiles/OU_GIT.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OU_GIT.dir/main.c.obj"
+  "OU_GIT.exe"
+  "OU_GIT.exe.manifest"
+  "OU_GIT.pdb"
+  "libOU_GIT.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/OU_GIT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
