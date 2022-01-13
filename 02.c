902a65ad8370459cb07b330126eb3999be88bcad
NightRaven <240697@edu.p.lodz.pl>
2022-01-13 20:31:39 +0100

    Suma zamieniona na mnożenie

--- 02.c
+++ 02.c
@@ -4,11 +4,11 @@ int Policz(int a, int b);
 
 int Policz(int a, int b)
 {
-    return a+b;
+    return a*b;
 }
 int main(void)
 {
     printf("Hello, World!\n");
-    printf("W pierwszym branchu; 2+3=%d",Policz(2,3));
+    printf("W pierwszym branchu; 2*3=%d",Policz(2,3));
     return 0;
 }
