24870885b1296d6ec02c7b13350578f8e7349d42
NightRaven <240697@edu.p.lodz.pl>
2022-01-13 20:28:31 +0100

    Hello World

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main() {
+int Policz(int a, int b);
+
+int Policz(int a, int b)
+{
+    return a+b;
+}
+int main(void)
+{
     printf("Hello, World!\n");
+    printf("W pierwszym branchu; 2+3=%d",Policz(2,3));
     return 0;
 }
